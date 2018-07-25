990e39d7a3b5ec85c835bdcfb5bf687636e19b29
Joshua Jancula <josh.jancula@sebrio.com>
2018-07-24 23:26:51 -0400

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule, MatSelectModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule } from '@angular/material';
+import { MatSidenavModule, MatButtonModule, MatDialogModule, MatListModule, MatSnackBarModule, MatIconModule } from '@angular/material';
+import { MatMenuModule, MatToolbarModule, MatTableModule, MatPaginatorModule, MatCheckboxModule, MatChipsModule } from '@angular/material';
+import { MatSlideToggleModule, MatRadioModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpModule,
+    RouterModule.forRoot([ ])
   ],
   providers: [],
   bootstrap: [AppComponent]
